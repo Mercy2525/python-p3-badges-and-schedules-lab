1665af5591b4f255131418646860a30a7676878a
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-21 02:14:27 +0300

    progress

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,8 +1,15 @@
 def badge_maker(name):
-    return None
+    return (f'Hello, my name is {name}.')
+badge_maker('Arel')
 
 def batch_badge_creator(names):
-    return None
+    my_list=[]
+    for name in names:
+        my_list.append(f"Hello, my name is {name}.")
+    return print( my_list)
+
+batch_badge_creator(['Mary','me','her'])
+    
 
 def assign_rooms(names):
     return None
